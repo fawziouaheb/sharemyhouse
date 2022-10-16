607cf12a760e08de69482e64e888f63a363ec97f
fawziouaheb <113803014+fawziouaheb@users.noreply.github.com>
2022-10-16 10:22:41 +0200

    Delete sharemyhouse directory

--- sharemyhouse/config1.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-$bdd= mysqli_connect("localhost","root","","house");
-if ($bdd->connect_error) {
-    echo 'Erreur de connexion (' . $bdd->connect_errno . ') ' . $bdd->connect_error;
-}
-?>
